0db53f0f755e125f7b63fb9d5321b96bf039645a
Michael Ruoss <michael@michaelruoss.ch>
2024-03-21 14:33:09 +0100

    add sponsorship link

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule FlameK8sBackend.MixProject do
       maintainers: ["Michael Ruoss"],
       licenses: ["MIT"],
       links: %{
-        "GitHub" => @source_url
+        "GitHub" => @source_url,
+        "Sponsor" => "https://github.com/sponsors/mruoss"
       },
       files: ["lib", "mix.exs", "README*", "LICENSE*", "CHANGELOG.md"]
     ]
